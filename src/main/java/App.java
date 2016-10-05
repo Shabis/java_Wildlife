2e86f954d39aa11a48a65010541b996ca13ae1fa
Shelby Clayton <shelby_clayton@hotmail.com>
2016-10-04 23:40:44 -0700

    add initial setup and root route in App.java

---
+++ src/main/java/App.java
@@ -0,0 +1,21 @@
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import spark.ModelAndView;
+import java.sql.Timestamp;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+public class App {
+  public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+  }
+}
