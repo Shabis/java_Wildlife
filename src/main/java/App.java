d7a376c09c5caf4d4c41e1575aee862ccbd770c6
Shelby Clayton <shelby_clayton@hotmail.com>
2016-10-04 23:48:30 -0700

    add routing to both animal pages and sighting.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,5 +17,28 @@ public class App {
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    get("/animal", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("animal", Animal.all());
+      model.put("template", "templates/animal.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/endangered", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("endangered", EndangeredAnimal.all());
+      model.put("template", "templates/endangered.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/sighting", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("sighting", Sighting.all());
+      model.put("animals", Animal.all());
+      model.put("template", "templates/sighting.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
   }
 }
